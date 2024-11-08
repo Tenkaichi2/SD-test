a603e8010f49ebf603320e8d43d4dd36df3d4441
Tenkaichi2 <51246526+Tenkaichi2@users.noreply.github.com>
2024-11-08 13:12:03 +0100

    Update default.sh

--- default.sh
+++ default.sh
@@ -15,14 +15,14 @@ PIP_PACKAGES=(
 )
 
 CHECKPOINT_MODELS=(
-    "https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.ckpt"
+    "https://civitai.com/api/download/models/646313?type=Model&format=SafeTensor&size=pruned&fp=fp16&token=80ba4ce83615265be2aee789b28f88da"
     #"https://huggingface.co/stabilityai/stable-diffusion-2-1/resolve/main/v2-1_768-ema-pruned.ckpt"
     #"https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors"
     #"https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0.safetensors"
 )
 
 LORA_MODELS=(
-    "https://civitai.com/api/download/models/16576"
+    "https://civitai.com/api/download/models/16576&token=80ba4ce83615265be2aee789b28f88da"
 )
 
 
